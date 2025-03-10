2acf10607e7e9a24e0f52f9e6fdde21bca42042b
KamilCoolas <62995217+KamilCoolas@users.noreply.github.com>
2025-03-10 13:33:16 +0100

    Create Game Dropdowns Refactor

---
+++ Assets/Scripts/CsvFileParsing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal static class CsvFileParsing
+    {
+        public static string[,] ParseTextAsset(TextAsset ft)
+        {
+            string fs = ft.text;
+            string[] fLines = Regex.Split(fs, "\r\n");
+            List<string> lines = new List<string>();
+            foreach (string t in fLines)
+            {
+                if (t != "")
+                {
+                    lines.Add(t);
+                }
+            }
+            string[,] values = new string[lines.Count, 3];
+            int k = 0;
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string valueLine = lines[i];
+                string[] splitet = Regex.Split(valueLine, ",");
+                int j = 1;
+                values[k, 0] = k.ToString();
+                foreach (string t in splitet)
+                {
+                    values[k, j] = t;
+                    j++;
+                }
+                k++;
+            }
+            return values;
+        }
+    }
+}
--- Assets/Scripts/NewGameValuation.cs
+++ Assets/Scripts/NewGameValuation.cs
@@ -1,124 +1,80 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.IO;
-using Unity.VisualScripting.FullSerializer;
-using System.Text.RegularExpressions;
-using UnityEditor.Experimental.GraphView;
 using System;
 
 public class NewGameValuation : MonoBehaviour
 {
     public TextAsset genreList;
-    public TMP_Text themeDropdown;
-    public TMP_Text graphicsDropdown;
+    public TMP_Dropdown genreDropdown;
+    public TextAsset themeList;
+    public TMP_Dropdown themeDropdown;
+    public TextAsset graphicList;
+    public TMP_Dropdown graphicDropdown;
     public TMP_Text estDur;
     public TMP_Text estCost;
-    public TMP_Dropdown genreDropdown;
+    public Button CreateButton;
+    public TMP_InputField TitleInput;
     int genreValue;
     int themeValue;
     int graphicValue;
     public static int estDurValue;
     public static int estCostValue;
     string[,] genreListArray;
-    //    {
-    //          "Arcade",
-    //          "Endless Runner",
-    //          "Platformer",
-    //          "RPG"
-    //    };
-    Dictionary<string, int> genre = new();
-
-
-    // Adding elements
-
+    Dictionary<string, int> genreId = new();
+    string[,] themeListArray;
+    Dictionary<string, int> themeId = new();
+    string[,] graphicListArray;
+    Dictionary<string, int> graphicId = new();
     private void Start()
     {
-        genreListArray = ParseTextAsset(genreList);
-        //foreach (string t in genreListArray)
-        for (int i = 0; i < genreListArray.GetLength(0); i++)
-        {
-            genreDropdown.options.Add(new TMP_Dropdown.OptionData() { text = genreListArray[i,0] });
-            genre.Add(genreListArray[i, 0], Convert.ToInt32(genreListArray[i, 1]));
-        }
-        genreDropdown.onValueChanged.AddListener(delegate { DropdownValueChanged(genreDropdown); });
-    }
-    void DropdownValueChanged(TMP_Dropdown change)
-    {
-        genreValue = genre[change.captionText.text];
+        LoadingDropdownValues("genre", genreList, genreDropdown, genreListArray, genreId);
+        LoadingDropdownValues("theme", themeList, themeDropdown, themeListArray, themeId);
+        LoadingDropdownValues("graphic", graphicList, graphicDropdown, graphicListArray, graphicId);
+        TitleInput.onValueChanged.AddListener(delegate { RecalculateValues(); });
     }
     void Update()
     {
-        
-        //switch (genreDropdown.captionText.text)
-        //{
-        //    case "Arcade":
-        //        genreValue = 1;
-        //        break;
-        //    case "Endless Runner":
-        //        genreValue = 1;
-        //        break;
-        //    case "Platformer":
-        //        genreValue = 2;
-        //        break;
-        //}
-        switch (themeDropdown.text)
-        {
-            case "Space":
-                themeValue = 1;
-                break;
-            case "Pirates":
-                themeValue = 1;
-                break;
-            case "Fantasy":
-                themeValue = 1;
-                break;
-        }
-        switch (graphicsDropdown.text)
-        {
-            case "2D":
-                graphicValue = 1;
-                break;
-            case "2.5D":
-                graphicValue = 2;
-                break;
-            case "3D":
-                graphicValue = 3;
-                break;
-        }
-        estDurValue = genreValue + themeValue + graphicValue;
-        estCostValue = (100 * genreValue + 100 * themeValue + 100 * graphicValue)*estDurValue;
-        estDur.text = "Estimated Duration: " + estDurValue + " turn(s)";
-        estCost.text = "Estimated Cost: " + estCostValue + "$";
     }
-    public string[,] ParseTextAsset(TextAsset ft)
+    
+    void RecalculateValues()
     {
-        string fs = ft.text;
-        string[] fLines = Regex.Split(fs, "\r\n");
-        List<string> lines = new List<string>();
-        foreach (string t in fLines)
+        if (genreValue != 0 && themeValue != 0 && graphicValue != 0 && TitleInput.text != "")
         {
-            if (t != "")
-            {
-                lines.Add(t);
-            }
+            estDurValue = genreValue + themeValue + graphicValue;
+            estCostValue = (100 * genreValue + 100 * themeValue + 100 * graphicValue) * estDurValue;
+            estDur.text = "Estimated Duration: " + estDurValue + " turn(s)";
+            estCost.text = "Estimated Cost: " + estCostValue + "$";
+            CreateButton.interactable = true;
         }
-        string[,] values = new string[lines.Count, 2];
-        int k = 0;
-        for (int i = 0; i < lines.Count; i++)
+        else CreateButton.interactable = false;
+    }
+    void LoadingDropdownValues(string type, TextAsset file, TMP_Dropdown dropdown, string[,] ListArray, Dictionary<string, int> Id)
+    {
+        ListArray = Assets.Scripts.CsvFileParsing.ParseTextAsset(file);
+        for (int i = 0; i < ListArray.GetLength(0); i++)
         {
-                string valueLine = lines[i];
-                string[] splitet = Regex.Split(valueLine, ","); // your splitter here
-                int j = 0;
-                foreach (string t in splitet)
-                {
-                    values[k,j] = t;
-                    j++;
-                }
-                k++;
-            }
-        return values;
+            dropdown.options.Add(new TMP_Dropdown.OptionData() { text = ListArray[i, 1] });
+            Id.Add(ListArray[i, 1], Convert.ToInt32(ListArray[i, 0]));
+        }
+        dropdown.onValueChanged.AddListener(delegate { DropdownValueChanged(type, dropdown, ListArray, Id); });
+    }
+    void DropdownValueChanged(string type, TMP_Dropdown change, string[,] ListArray, Dictionary<string, int> Id)
+    {
+        int value = Convert.ToInt32(ListArray[Id[change.captionText.text], 2]);
+        switch (type)
+        {
+            case "genre":
+                genreValue = value;
+                break;
+            case "theme":
+                themeValue = value;
+                break;
+            case "graphic":
+                graphicValue = value;
+                break;
+        }
+        RecalculateValues();
     }
 }
